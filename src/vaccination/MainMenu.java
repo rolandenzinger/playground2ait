0afd0debb045f4ff055cf73a50f7bcac0a89beca
Roland <enzinger@parkside.at>
2021-03-10 23:27:59 +0100

    changing the name of the class

--- src/vaccination/MainMenu.java
+++ src/vaccination/MainMenu.java
@@ -9,7 +9,7 @@ public class MainMenu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello 2BIT");
+		System.out.println("Hello 2AIT");
 		
 		// put your code here
 
