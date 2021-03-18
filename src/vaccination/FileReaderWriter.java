8467a3d641fa7f3ac88f3f0ca58d844ca80d4be9
Megannix <80470133+Megannix@users.noreply.github.com>
2021-03-18 16:57:59 +0100

    text

--- src/vaccination/FileReaderWriter.java
+++ src/vaccination/FileReaderWriter.java
@@ -48,11 +48,13 @@ public class FileReaderWriter {
 		
 	
 	
-	public String fileReading(String infoBlock, int key, int id) {
+	public String fileReading(int key, int id) {
 		
 		//Elias
 
 		//Das ist ein Test
+		
+		String infoBlock;
 		
 		infoBlock = info1 +";"+ info2 +";"+ info3 +";"+ info4 +";"+ info5 +";"+ info6 +";"+ info7 +";"+ info8 +";"+ info9 +";"+ info10;
    		return infoBlock;
