112346a3565ef824fca511838278ed1802624118
nuklear10000 <56518296+nuklear10000@users.noreply.github.com>
2021-03-18 15:40:40 +0100

    Update FileReaderWriter.java

--- src/vaccination/FileReaderWriter.java
+++ src/vaccination/FileReaderWriter.java
@@ -1,8 +1,34 @@
 package vaccination;
 
 public class FileReaderWriter {
+	private int key;
+	private int id;
+	private String info1;
+	private String info2;
+	private String info3;
+	private String info4;
+	private String info5;
+	private String info6;
+	private String info7;
+	private String info8;
+	private String info9;
+	private String info10;
 	
-	
+	FileReaderWriter(int key, int id , String info1, String info2,String info3,String info4,String info5,String info6,String info7,String info8,String info9,String info10){
+		this.key = key;
+		this.id = id;
+		this.info1 = info1;
+		this.info2 = info2;
+		this.info3 = info3;
+		this.info4 = info4;
+		this.info5 = info5;
+		this.info6 = info6;
+		this.info7 = info7;
+		this.info8 = info8;
+		this.info9 = info9;
+		this.info10 = info10;
+		
+	}
 	
 	public void fileWriting() {
 		
