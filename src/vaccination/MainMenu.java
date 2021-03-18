214f1bc9066994fa5a99b550d2a030f364ee3d61
Maverick <tobi.berger1@gmx.at>
2021-03-18 16:12:20 +0100

    first main

--- src/vaccination/MainMenu.java
+++ src/vaccination/MainMenu.java
@@ -1,18 +1,56 @@
 package vaccination;
+//collab mit UI:
+
+// String = UserInterface.AskForString(String);
+// void UserInterface.SaySomething();
 
 public class MainMenu {
 
 	public MainMenu() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello 2AIT");
-		
-		// put your code here
+		// Main UI
+		boolean quit = false;
+		String InterfaceOutput = 
+		"Welcome to VacManager:\n
+		------------------------
+		Standortverwaltung    [ort]
+		Impf-Zeitfenster      [zeit]
+		Terminvergabe         [termin]
+		Personalverwaltung    [person]
+		Über diese Applikation[über]
+		Beenden               [Beenden]
+		";
+		do {
+			switch (UserInterface.AskForString(InterfaceOutput)) {
+			case "ort":
+				location();
+				break;
+
+			case "zeit":
+				timeslot();
+				break;
+			
+			case "termin":
+				scedule();
+				break;
+
+			case "person":
+				personal();
+				break; 
+			
+			case "über":
+				about();
+				break;
+			
+			case "beenden":
+				quit = true; 
+			default:
+			UserInterface.SaySomething("Kein Eintrag unter diesem Namen");
+			}
+		} while (!quit);
 
 	}
-
 }
