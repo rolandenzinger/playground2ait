162826b6ff29a35cffc1a6a4d63a943a79e9179e
RedBread22 <191wita02@o365.htl-leoben.at>
2021-03-18 16:46:33 +0100

    hallo

--- src/vaccination/MainMenu.java
+++ src/vaccination/MainMenu.java
@@ -9,10 +9,55 @@ public class MainMenu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello 2AIT");
-		
 		// put your code here
 
 	}
+		public void TimeSlot() {
+			do {
+				switch(UserInterFace.AskForString("Eingabe: \n Löschen Eingeben")) {
+ 					case "";
 
+					addTime();
+
+					// Methode "einfügen" aufrufen
+					break;
+				}
+				case "";
+
+				deleteTime();
+
+				// Methode "löschen" aufrufen
+					break;
+
+
+			} while (!quit);
+		}
+		public void Person() {
+			do {
+				switch(UserInterFace.AskForString("Eingabe: \n Person anlegen 
+															\n Person löschen
+															\n Alle Personen listen")) {
+ 					case "";
+
+					addPerson();
+
+					// Methode "einfügen" aufrufen
+					break;
+				}
+				case "";
+
+				deletePerson();
+
+				// Methode "löschen" aufrufen
+					break;
+				case "";
+
+				listPerson();
+
+				// Methode "löschen" aufrufen
+					break;
+
+
+			} while (!quit);
+		}
 }
