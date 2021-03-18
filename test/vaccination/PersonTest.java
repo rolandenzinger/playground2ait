e057b46522ebcf3e1035352c3b35b639dd818c15
marco22st <191wita24@o365.htl-leoben.at>
2021-03-18 17:07:46 +0100

    Update PersonTest.java

--- test/vaccination/PersonTest.java
+++ test/vaccination/PersonTest.java
@@ -9,7 +9,10 @@ class PersonTest {
 	@Test
 	void test() {
 		Person one = new Person();
-		assertEquals("556363" , one.addNewPerson("556363", "Max", "Mustermann", 12345, "Straße", "1234", "Stadt", "Land", "12.34.5678", 12345678));
+		assertEquals("556363", one.addNewPerson("556363", "Max", "Mustermann", 12345, "Straße", "1234", "Stadt", "Land",
+				"12.34.5678", 12345678));
+		one.listAllPersons();
+		one.deletePerson("556363");
 	}
 
 }
