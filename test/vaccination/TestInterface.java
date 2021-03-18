863958b1c515b3a4c1312e79e52feeb185ebf261
LucaL <LucaL@Luca-PC>
2021-03-18 15:57:16 +0100

    TestCasés

---
+++ test/vaccination/TestInterface.java
@@ -0,0 +1,43 @@
+package vaccination;
+
+import org.junit.Test;
+
+public class TestInterface {
+
+	   @Test public void TestSaySmth() {
+	    	
+		   
+		   
+          //asserEquals(UserInterface.SaySomething("hello"));
+
+
+   }
+	   
+	   @Test public void TestAskForString() {
+	    	
+		   
+		   
+           //asserEquals(UserInterface.SaySomething("hello"));
+
+
+   }
+	   
+	   @Test public void TestAskForDate() {
+	    	
+		   
+		   
+		   //    asserEquals(UserInterface.SaySomething("hello"));
+
+
+   }
+	   
+	   @Test public void TestAskForNumber() {
+	    	
+		   
+		   
+		   //       asserEquals(UserInterface.SaySomething("hello"));
+
+
+   }
+	
+}
