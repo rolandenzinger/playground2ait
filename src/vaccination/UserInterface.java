630f745d7a319498835c511c0b149b74f2d0a6ef
LucaL <LucaL@Luca-PC>
2021-03-18 15:28:59 +0100

    Test

--- src/vaccination/UserInterface.java
+++ src/vaccination/UserInterface.java
@@ -1,5 +1,36 @@
 package vaccination;
 
+import java.sql.Date;
+
 public class UserInterface {
 
+	public void SaySomething(String Info) {
+		
+		
+		
+		
+	}
+	
+	public String AskForString ( ) {
+		return null;
+		
+		
+		
+		
+	}
+	
+	public Date AskForDate ( ) {
+		return null;
+		
+		
+		
+	}
+	
+	public int AskForNumber ( ) {
+		return 0;
+		
+		
+		
+	}
+	
 }
