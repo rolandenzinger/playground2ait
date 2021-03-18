a63088ae7e552411fa56556b345853733ebf5cdc
marco22st <191wita24@o365.htl-leoben.at>
2021-03-18 15:33:00 +0100

    Update Person.java

--- src/vaccination/Person.java
+++ src/vaccination/Person.java
@@ -12,8 +12,16 @@ public class Person {
 
     public String addNewPerson(String personID,String firstName,String lastName,int socialSecurityNumber,String street,String zip,String city,String country,String birthday,int telephone) {
         this.personID = personID;
-
-        return "";
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this. street = street;
+        this.zip = zip;
+        this.city = city;
+        this.country = country;
+        this.birthday = birthday;
+        this.telephone = telephone;
+        return personID;
     }
 
     public void deletePerson(personID) {
