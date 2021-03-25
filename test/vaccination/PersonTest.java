139b9113f604c6abe1e6add5515eacf6fce62aab
marco22st <191wita24@o365.htl-leoben.at>
2021-03-25 15:20:28 +0100

    Update PersonTest.java

--- test/vaccination/PersonTest.java
+++ test/vaccination/PersonTest.java
@@ -9,13 +9,13 @@ class PersonTest {
 	@Test
 	void test() {
 		Person one = new Person();
-		assertEquals("556363", one.addNewPerson("556363", "Max", "Mustermann", 12345, "Straße", "1234", "Stadt", "Land",
-				"12.34.5678", 12345678));
+		assertEquals("556363", one.addNewPerson("556363", "Max", "Mustermann", "12345", "Straße", "1234", "Stadt", "Land",
+				"12.34.5678", "12345678"));
 		one.deletePerson("556363");
-		assertEquals("556364", one.addNewPerson("556364", "Max", "Mustermann", 12345, "Straße", "1234", "Stadt", "Land",
-				"12.34.5678", 12345678));
-		assertEquals("556365", one.addNewPerson("556365", "Max", "Mustermann", 12345, "Straße", "1234", "Stadt", "Land",
-				"12.34.5678", 12345678));
+		assertEquals("556364", one.addNewPerson("556364", "Max", "Mustermann", "12345", "Straße", "1234", "Stadt", "Land",
+				"12.34.5678", "12345678"));
+		assertEquals("556365", one.addNewPerson("556365", "Max", "Mustermann", "123452", "Straße", "1234", "Stadt", "Land",
+				"12.34.5678", "12345678"));
 		one.listAllPersons();
 		one.deletePerson("556365");
 	}
