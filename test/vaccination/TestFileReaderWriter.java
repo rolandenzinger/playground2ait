3a06c45d2ad5cc08e33b8dc0d03a5ea37497f4db
nuklear10000 <56518296+nuklear10000@users.noreply.github.com>
2021-03-25 17:34:27 +0100

    Update TestFileReaderWriter.java

--- test/vaccination/TestFileReaderWriter.java
+++ test/vaccination/TestFileReaderWriter.java
@@ -31,7 +31,7 @@ class TestFileReaderWriter {
 		 String info7 = "Keine Kinder";
 		 String info8 = "Ledich";
 		 String info9 = "32";
-		 String info10 = "Männlich";
+		 String info10 = "Maennlich";
 		FileReaderWriter test = new FileReaderWriter(key,id,info1,info2,info3,info4,info5,info6,info7,info8,info9,info10);
 		test.fileWriting(test);
 		String readingResult = "";
