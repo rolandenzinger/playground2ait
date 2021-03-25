67f807c912309e475152aefb685b99e3e9a848e6
unknown <Luksizockt@gmail.com>
2021-03-25 16:01:28 +0100

    t

--- src/vaccination/FileReaderWriter.java
+++ src/vaccination/FileReaderWriter.java
@@ -1,100 +1,103 @@
 package vaccination;
 
+import javax.management.relation.RoleInfoNotFoundException;
 import java.io.*;
 
 public class FileReaderWriter {
-	private String key;
-	private int id;
-	private String info1;
-	private String info2;
-	private String info3;
-	private String info4;
-	private String info5;
-	private String info6;
-	private String info7;
-	private String info8;
-	private String info9;
-	private String info10;
+    private String key;
+    private int id;
+    private String info1;
+    private String info2;
+    private String info3;
+    private String info4;
+    private String info5;
+    private String info6;
+    private String info7;
+    private String info8;
+    private String info9;
+    private String info10;
 
-	FileReaderWriter(String key, int id , String info1, String info2,String info3,String info4,String info5,String info6,String info7,String info8,String info9,String info10){
-		this.key = key;
-		this.id = id;
-		this.info1 = info1;
-		this.info2 = info2;
-		this.info3 = info3;
-		this.info4 = info4;
-		this.info5 = info5;
-		this.info6 = info6;
-		this.info7 = info7;
-		this.info8 = info8;
-		this.info9 = info9;
-		this.info10 = info10;
+    FileReaderWriter(String key, int id, String info1, String info2, String info3, String info4, String info5, String info6, String info7, String info8, String info9, String info10) {
+        this.key = key;
+        this.id = id;
+        this.info1 = info1;
+        this.info2 = info2;
+        this.info3 = info3;
+        this.info4 = info4;
+        this.info5 = info5;
+        this.info6 = info6;
+        this.info7 = info7;
+        this.info8 = info8;
+        this.info9 = info9;
+        this.info10 = info10;
 
-	}
+    }
 
-	public void fileWriting(String infoBlock)
+    public void fileWriting(String infoBlock)
 
-			 throws IOException {
-				 String idstring = String.valueOf(id);
+            throws IOException {
+        String idstring = String.valueOf(id);
 
-	    BufferedWriter writer = new BufferedWriter(new FileWriter("Impfstoff", true));
-	    writer.append(idstring);
-	    writer.append(key);
-	    writer.append(info1);
-	    writer.append(info2);
-	    writer.append(info3);
-	    writer.append(info4);
-	    writer.append(info5);
-	    writer.append(info6);
-	    writer.append(info7);
-	    writer.append(info8);
-	    writer.append(info9);
-	    writer.append(info10);
-
-
-	    writer.close();
-	}
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Impfstoff", true));
+        writer.append(idstring);
+        writer.append(key);
+        writer.append(info1);
+        writer.append(info2);
+        writer.append(info3);
+        writer.append(info4);
+        writer.append(info5);
+        writer.append(info6);
+        writer.append(info7);
+        writer.append(info8);
+        writer.append(info9);
+        writer.append(info10);
 
 
+        writer.close();
+    }
 
 
+    public String fileReading(String key, int id) {
 
-	public String fileReading(String key, int id) {
-
-		//Elias
-
-
-		return null;
-	}
-
-	public String[] ReadAllEntriesWithKey(String key){
-		String line;
-		String[] arr = new String[10];
-		int i = 0;
-
-		try {
-			BufferedReader read = new BufferedReader(new FileReader("./Impfung.xlsx"));
-			System.out.println("File read!");
-			if (read.readLine().equalsIgnoreCase(key)) {
-				while(!(read.readLine().isEmpty())){
-					arr[i] = read.readLine();
-					i++;
-				}
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return arr;
-	}
-
-	public String GetInfoFromBlock(String infoBlock, int IndexOfRequestedInformation){
+        //Elias
 
 
-		String info = null;
-		return info;
-	}
+        return null;
+    }
+
+    public String[] ReadAllEntriesWithKey(String key) {
+        String line;
+        String[] arr = new String[12];
+        int i = 0;
+
+        try {
+            BufferedReader read = new BufferedReader(new FileReader("./Impfung.csv"));
+            System.out.println("File read!");
+            String currentLine = read.readLine();
+            while (!(currentLine.isEmpty())) {
+                if (currentLine.startsWith(key)) {
+                    arr[i] = currentLine;
+                    i++;
+                }
+                currentLine = read.readLine();
+            }
+            read.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return arr;
+    }
+
+    public String GetInfoFromBlock(String infoBlock, int IndexOfRequestedInformation) {
+        String info = null;
+        String[] arr = new String[12];
+        arr = infoBlock.split(";");
+        info = arr[IndexOfRequestedInformation];
+
+        return info;
+    }
 
 }
