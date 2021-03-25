54ee4a64a058259e34bd846ec0a864dc64be6ce6
Markus O <Markus O@LAPTOP-FIINNNV6>
2021-03-25 16:30:27 +0100

    Hallo

--- src/vaccination/TimeSlots.java
+++ src/vaccination/TimeSlots.java
@@ -20,43 +20,56 @@ public class TimeSlots {
 		schedules = new String[0];
 	}
 
-	public int AddNewSlot(int slotId, int locationId, int personId, String date, String timeStart, String timeEnd) {
+	public int AddNewSlot(int locationId, String date, String timeStart, String timeEnd) {
 
 		String[] tmp = schedules.clone();
 		schedules = new String[schedules.length + 1];
 		slotId++;
 
-		for (int i = 0; i < tmp.length; i++) {
+		for (int i = 0; i < schedules.length; i++) {
 
 			if (i < schedules.length - 1) {
 				schedules[i] = tmp[i];
 			} else {
-				schedules[i] = slotId + ";" + locationId + ";" + personId + ";" + date + ";" + timeStart + ";"
-						+ timeEnd;
+				schedules[i] = slotId + ";" + locationId + ";" + "---" + ";" + date + ";" + timeStart + ";" + timeEnd
+						+ ";" + "false";
 			}
 
 		}
 
 		return slotId;
 	}
-	
-	public boolean DeleteUnscheduledSlot(int slotId) {
+
+	public boolean DeleteUnscheduledSlot(int slotId, boolean slotIsScheduled) {
+
+		for (int i = 0; i < schedules.length; i++) {
+
+			String[] tmp = schedules[i].split(";");
+
+			if (tmp[7].equals("false")) {
+				
+
+				schedules = tmp;
+			}
+
+		}
+
 		return true;
 	}
-	
+
 	public int AddScheduledSlotForPerson(int locationId, int personId, int slotId) {
 		return scheduleId;
 	}
-	
+
 	public boolean DeleteScheduledSlotForPerson(int scheduleId) {
 		return true;
 	}
-	
-	public String[] getAllUnscheduledSlotsForStation(int stationId) {
+
+	public int[] getAllUnscheduledSlotsForStation(int stationId) {
 		return null;
 	}
-	
-	public String[] GetAllUnscheduledSlotsForDate(String date) {
+
+	public int[] GetAllUnscheduledSlotsForDate(String date) {
 		return null;
 	}
 }
